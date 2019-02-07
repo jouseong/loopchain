1d3a1d9dd073c4b513540c3698f66b0e8a3a3f24
Gyeong-Rok.Lee <sohalee@icon.foundation>
2019-02-07 17:20:45 +0900

    [LC-152] Remove pickle to communicate between peers
    Signed-off-by: Gyeong-Rok.Lee <sohalee@icon.foundation>

--- loopchain/baseservice/broadcast_scheduler.py
+++ loopchain/baseservice/broadcast_scheduler.py
@@ -131,7 +131,6 @@ class BroadcastScheduler(CommonThread):
         """등록된 모든 Peer 의 동일한 gRPC method 를 같은 파라미터로 호출한다.
         """
         # logging.warning("broadcast in process ==========================")
-        # logging.debug("pickle method_param: " + str(pickle.dumps(method_param)))
 
         kwargs = {}
         if retry_times is not None:
